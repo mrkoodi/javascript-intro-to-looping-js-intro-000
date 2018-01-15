9ad38116e869a12ef3a5e2a2f554ed0b393816cd
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-15 20:56:01 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,8 @@ function forLoop(array) {
   for (var i = 0; i > 25; i++) {
     if (i === 1) {
       return "I am" + i + "strange loop";
+    } else if (i === 0) {
+      return `I am ${i} strange loops`;
     }
   }
 
